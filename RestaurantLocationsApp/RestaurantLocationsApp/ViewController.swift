97051b6fd981347a53989fab43b273eb55816a88
Jordi Farras Mañe <jfarras8@hotmail.com>
2019-02-06 12:00:56 +0100

    base project

---
+++ RestaurantLocationsApp/RestaurantLocationsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantLocationsApp
+//
+//  Created by Jordi Farras Mañe on 06/02/2019.
+//  Copyright © 2019 Jordi Farras Mañe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
