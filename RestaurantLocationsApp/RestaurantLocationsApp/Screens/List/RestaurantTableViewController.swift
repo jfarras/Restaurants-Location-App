ca4cbcee10296ee81ddff8fc954c5a733ea8ae93
Jordi Farràs <jfarras8@hotmail.com>
2019-03-01 13:39:18 +0100

    change

--- RestaurantLocationsApp/RestaurantLocationsApp/Screens/List/RestaurantTableViewController.swift
+++ RestaurantLocationsApp/RestaurantLocationsApp/Screens/List/RestaurantTableViewController.swift
@@ -19,6 +19,7 @@ class RestaurantTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
 
     // MARK: - Table view data source
 
